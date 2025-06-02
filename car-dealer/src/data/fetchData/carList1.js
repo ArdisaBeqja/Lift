7dce84adb41fbcba9e70ceb8a9aee0c43b31a83f
ArdisaBeqja <abeqja21@epoka.edu.al>
2025-06-02 13:40:34 +0200

    changed fetch api

--- car-dealer/src/data/fetchData/carList1.js
+++ car-dealer/src/data/fetchData/carList1.js
@@ -4,7 +4,9 @@ const useCarList = () => {
   const [cars, setCars] = useState([]);
 
   useEffect(() => {
-    fetch("http://localhost:8000/api/cars")
+    // fetch("http://localhost:8000/api/cars")
+      fetch(`${process.env.REACT_APP_API_URL}/api/cars`)
+
       .then((response) => response.json())
       .then((data) => setCars(data))
       .catch((error) => console.error("Error fetching cars:", error));
