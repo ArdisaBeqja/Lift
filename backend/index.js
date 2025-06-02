f83cfc42be74836a47590032033cc69c6423752f
ArdisaBeqja <abeqja21@epoka.edu.al>
2025-06-02 15:58:21 +0200

    chaged port to 8000

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ const __dirname = path.dirname(__filename);
 
 
 const app = express();
-const port = 10000;  // Use Render's assigned port, fallback to 8000 locally
+const port = 8000;  // Use Render's assigned port, fallback to 8000 locally
 
 
 // app.use(cors({
