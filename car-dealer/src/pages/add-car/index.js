e4a61b2e936fb92a8b1d23ce83972a5ff848f3f8
ArdisaBeqja <abeqja21@epoka.edu.al>
2025-06-03 23:25:10 +0200

    changed addcar -> add-car

--- car-dealer/src/pages/add-car/index.js
+++ car-dealer/src/pages/add-car/index.js
@@ -158,7 +158,7 @@ function AddCar() {
     });
 
     try {
-      const response = await axios.post("https://cardealeral.onrender.com/addcar", formData, {
+      const response = await axios.post("https://cardealeral.onrender.com/add-car", formData, {
         headers: { 'Content-Type': 'multipart/form-data' },
       });
       console.log('Response:', response.data);
