a01702ffdd2795fbe4e0b314da5a7a0951b45675
ArdisaBeqja <abeqja21@epoka.edu.al>
2025-06-03 17:47:13 +0200

    faster mp4

--- car-dealer/src/pages/modern-home1/video-counter.js
+++ car-dealer/src/pages/modern-home1/video-counter.js
@@ -1,31 +1,32 @@
 // Libraries
 import { Col, Container, Row } from 'react-bootstrap';
-import Vid from './audi.mp4';
-import Thumb from './thumb.avif';
+import Thumb from './thumb.avif'; // Keep the local image import
 // Components
 import VideoPopup from '../../component/video';
 
-function VideoCounter(params) {
+function VideoCounter() {
+  const cdnVideoURL = "https://cdn.jsdelivr.net/gh/ArdisaBeqja/mp4/audi.mp4";
+
   return (
-    <>
-      <Container>
-        <Row className="justify-content-center">
-          <Col lg={8} md={10} sm={12} className="video-btn">
-            <div className="video-box">
-              <VideoPopup
-                theme="video-style-2"
-                size="lg"
-                videoURL={Vid}
-                videoImage={Thumb}
-              />
-            </div>
-            <div className="section-title-new text-center">
-              <h3 className="title text-white mt-4">Want To Know More About? Play Our Promotional Video Now!</h3>
-            </div>
-          </Col>
-        </Row>
-      </Container>
-    </>
+    <Container>
+      <Row className="justify-content-center">
+        <Col lg={8} md={10} sm={12} className="video-btn">
+          <div className="video-box">
+            <VideoPopup
+              theme="video-style-2"
+              size="lg"
+              videoURL={cdnVideoURL}
+              videoImage={Thumb}
+            />
+          </div>
+          <div className="section-title-new text-center">
+            <h3 className="title text-white mt-4">
+              Want To Know More About? Play Our Promotional Video Now!
+            </h3>
+          </div>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
